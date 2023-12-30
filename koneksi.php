0e66169a8b9ba4c6169b1f4e3430684f5f359137
JoniCrash <iwan@comet.net.id>
2023-12-30 15:28:25 +0700

    namgah insert dan koneksi

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+	
+	$server		= "127.0.0.1"; // sesuaikan alamat server anda
+	$user		= "root"; // sesuaikan user web server anda
+	$password	= ""; // sesuaikan password web server anda
+	$database	= "kampus"; // sesuaikan database web server anda
+		
+	$con = mysqli_connect($server,$user,$password,$database);
+?>
